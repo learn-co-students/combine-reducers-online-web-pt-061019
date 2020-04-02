7cc644dd45bc84c8a38217c7af6ea7865b64d7a3
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-02 07:38:16 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
+import rootReducer from './reducers/manageAuthorsAndBooks';
+// import authorsReducer from './reducers/authorsReducer';
+// import booksReducer from './reducers/booksReducer';
 
+// const rootReducer = combineReducers({
+//   books: booksReducer,
+//   authors: authorsReducer
+// })
 
-const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+    <App store={store} />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
